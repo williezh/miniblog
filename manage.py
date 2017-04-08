1a9cd2b7ef7d821b7632f1f7d3aee9d3ef6251cc
williezh <276971837@qq.com>
2017-04-08 09:20:09 -0400

    add manage.py

---
+++ manage.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+#coding=utf-8
+
+import os, sys
+sys.path.append(os.path.abspath(os.path.dirname(__file__)))
+
+# from flask.ext.script import Manager, Server
+from flask_script import Manager, Server
+
+# from OctBlog import create_app
+# app = create_app(os.getenv('config') or 'default')
+
+from miniblog import app
+
+
+manager = Manager(app)
+
+# Turn on debugger by default and reloader
+manager.add_command("runserver", Server(
+    use_debugger = True,
+    use_reloader = True,
+    host = '0.0.0.0',
+    port = 5000)
+)
+
+@manager.command
+def test():
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+if __name__ == "__main__":
+    manager.run()
